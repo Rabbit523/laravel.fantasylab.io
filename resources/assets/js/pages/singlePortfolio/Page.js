06103d016ebdfeda5860e88fff1b9feab618b1a9
Nemanja <WorldHero2018@hotmail.com>
2019-09-16 21:34:10 +0200

    second test

--- resources/assets/js/pages/singlePortfolio/Page.js
+++ resources/assets/js/pages/singlePortfolio/Page.js
@@ -16,6 +16,7 @@ class Page extends React.Component {
 
     componentDidMount() {
         const { type } = this.props.match.params;
+        console.log(this.props);
         Http.post('api/front/get-portfolio-page', { type: type })
         .then(
             res => {
