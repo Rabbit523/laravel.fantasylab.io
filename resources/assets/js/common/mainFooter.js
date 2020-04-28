25ba86502168b97f321a774f4485dea6f9d36aa3
Dmitrii <dmitrii@Dmitriis-Mac.local>
2020-04-27 22:58:57 -0700

    update footer language section style

--- resources/assets/js/common/mainFooter.js
+++ resources/assets/js/common/mainFooter.js
@@ -139,7 +139,7 @@ class Footer extends React.Component {
 									<img src={isMobileOnly ? require('../../images/theme/logo.png') : require('../../images/theme/fantasy-logo.png')} />
 									<h5>© + TM 2020 FantasyLab AS, NO 922 982 376 MVA</h5>
 								</Grid.Column>
-								<Grid.Column mobile={16} tablet={4} computer={4} className="footer-lang" style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', padding: '10px 0px' }}>
+								<Grid.Column mobile={16} tablet={4} computer={4} className="footer-lang" style={{ display: 'flex', alignItems: 'center', padding: '10px 0px' }}>
 									<h5>{translate('footer.language')} </h5>
 									<Dropdown text={lang == 'en' ? 'English' : 'Norsk'}>
 										<Dropdown.Menu>
