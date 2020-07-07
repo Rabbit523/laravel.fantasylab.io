338584c91b2249e35068b39cf5718057730cadf9
Dmitrii <dmitrii@Dmitriis-Mac.local>
2020-07-07 05:34:55 -0700

    update order request template

--- resources/views/email/order-request.blade.php
+++ resources/views/email/order-request.blade.php
@@ -4,6 +4,6 @@
 <p><b>Company:</b>&nbsp;{{$company}}</p>
 <p><b>Email:</b>&nbsp;{{$email}}</p>
 <p><b>Phone:</b>&nbsp;{{$phone}}</p>
-<p><b>Message:</b>&nbsp;{{$message}}</p>
+<p><b>Message:</b>&nbsp;{{$description}}</p>
 <p><b>Agreement:</b>&nbsp;Enterprise Managed Hosting, kr 3 600,- excl. VAT pr. month</p>
 </html>
